95c64a41911b185a0571eea30275bd7ee057e8e7
Satyekumar <58253494+Satyekumar@users.noreply.github.com>
2020-01-30 16:18:34 +0530

    Add files via upload

---
+++ Untitled30.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+my_string="Satyendra kumar"
+string_lenth=len(my_string)
+print(string_lenth)
+
+
+# In[5]:
+
+
+ch="abra"
+ch1=" ka dabra"
+main_ch=ch+ch1
+print(main_ch)
+
+
+# In[11]:
+
+
+flavor="Birthday Cake"
+print(flavor[9])
+
+
+# In[18]:
+
+
+flavor="Birthday Cake"
+print(flavor[:])
+
+
+# In[23]:
+
+
+User_Input=input("Tell me your password= ")
+print("To unlock it..",User_Input)
+
+
+# In[24]:
+
+
+my_number=12
+print(int(my_number))
+
+
+# In[25]:
+
+
+my_number=12
+print(float(my_number))
+
+
+# In[28]:
+
+
+name="formatted"
+print(f"this is {name} string")
+
+
+# In[29]:
+
+
+phrase="The Suprise is here somewhere"
+print(phrase.find("Suprise"))
+
+
+# In[2]:
+
+
+my_story="I am telling you the truth;he spoke nothing but the truth!"
+print(my_story.replace("the truth","lies"))
+
+
+# In[ ]:
+
+
+
+
